bd2816795ff2a932d14cfc93aeffe467e4075e20
JiahaoZha <107592370+JiahaoZha@users.noreply.github.com>
2025-02-28 16:45:55 -0600

    REACT practing

--- REACT/REACT2.jsx
+++ REACT/REACT2.jsx
@@ -10,3 +10,68 @@ class MyComponent extends React.Component {
       )
     }
   };
+
+
+  const ChildComponent = () => {
+    return (
+      <div>
+        <p>I am the child</p>
+      </div>
+    );
+  };
+  
+  class ParentComponent extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <h1>I am the parent</h1>
+          <ChildComponent />
+        </div>
+      );
+    }
+  };
+
+
+  const TypesOfFruit = () => {
+    return (
+      <div>
+        <h2>Fruits:</h2>
+        <ul>
+          <li>Apples</li>
+          <li>Blueberries</li>
+          <li>Strawberries</li>
+          <li>Bananas</li>
+        </ul>
+      </div>
+    );
+  };
+  
+  const Fruits = () => {
+    return (
+      <div>
+
+        <TypesOfFruit />
+
+      </div>
+    );
+  };
+  
+  class TypesOfFood extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+  
+    render() {
+      return (
+        <div>
+          <h1>Types of Food:</h1>
+
+          <Fruits />
+
+        </div>
+      );
+    }
+  };
