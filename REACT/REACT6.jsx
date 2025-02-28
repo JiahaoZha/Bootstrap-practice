8330fe2b73d6437480403aa7e05966eb53172729
JiahaoZha <107592370+JiahaoZha@users.noreply.github.com>
2025-02-28 17:07:12 -0600

    REACT practing

--- REACT/REACT6.jsx
+++ REACT/REACT6.jsx
@@ -49,3 +49,13 @@ const CurrentDate = (props) => {
       );
     }
   };
+
+  const ShoppingCart = (props) => {
+    return (
+      <div>
+        <h1>Shopping Cart Component</h1>
+      </div>
+    )
+  };
+
+  ShoppingCart.defaultProps ={ items:0 }
