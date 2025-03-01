5cfdccabedd10b81fe2faff6a2c28c0729d36480
JiahaoZha <107592370+JiahaoZha@users.noreply.github.com>
2025-03-01 11:23:39 -0600

    REACT practicing

---
+++ REACT/REACT22.jsx
@@ -0,0 +1,13 @@
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    componentWillMount() {
+
+      console.log()
+
+    }
+    render() {
+      return <div />
+    }
+  };
